ff63f712ade13e711c07c0bfb5227cda51426774
teslmik <teslmik@gmail.com>
2023-06-09 10:08:30 +0300

    feat: Fix remarks

--- packages/frontend/src/modules/common/hooks/use-get-one-todo.ts
+++ packages/frontend/src/modules/common/hooks/use-get-one-todo.ts
@@ -2,16 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import todoService from '../../../services/todos.service';
 
-export const useGetOneTodo = (id: string) => {
-  const notify = (message: string) => toast(message);
-
-  return useQuery({
+export const useGetOneTodo = (id: string) =>
+  useQuery({
     queryFn: () => todoService.getOneTodo(id),
     queryKey: ['todo', id],
     onError: (err) => {
       if (err instanceof Error) {
-        notify(err.message);
+        toast.error(err.message);
       }
     }
   });
-};
