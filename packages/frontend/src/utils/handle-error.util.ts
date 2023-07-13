ab8320c9be60c03e5864f44325ac0ca6f6a2db2f
Mykhailo Teslenko <teslmik@gmail.com>
2023-07-13 18:07:04 +0300

    Update handle-error.util.ts

--- packages/frontend/src/utils/handle-error.util.ts
+++ packages/frontend/src/utils/handle-error.util.ts
@@ -1,9 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { AxiosResponse } from 'axios';
 import toast from 'react-hot-toast';
+import { logout } from './logout.util';
 
 export const handleError = (err: AxiosResponse) => {
-  if (err) {
-    toast.error(err.data.message);
+
+  if (err.status === 401) {
+    logout();
   }
+
+  toast.error(err.data.message);
 };
