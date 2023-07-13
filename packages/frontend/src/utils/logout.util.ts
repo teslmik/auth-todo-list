3dab589de9da420bff9dc2dcba1bae2c3537d79e
Mykhailo Teslenko <teslmik@gmail.com>
2023-07-13 18:16:44 +0300

    Update logout.util.ts

--- packages/frontend/src/utils/logout.util.ts
+++ packages/frontend/src/utils/logout.util.ts
@@ -1,6 +1,6 @@
-import { APP_KEYS } from "../modules/common/consts";
+import { APP_KEYS } from '../modules/common/consts';
 
 export const logout = () => {
   localStorage.removeItem(APP_KEYS.STORAGE_KEYS.TOKEN);
-  window.location.href = (APP_KEYS.ROUTER_KEYS.AUTH);
+  window.location.href = APP_KEYS.ROUTER_KEYS.AUTH;
 };
