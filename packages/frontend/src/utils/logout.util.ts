ed8e36523b3e2b972ed69d90b490427fafcacbc3
Mykhailo Teslenko <teslmik@gmail.com>
2023-07-13 18:07:58 +0300

    Create logout.util.ts

---
+++ packages/frontend/src/utils/logout.util.ts
@@ -0,0 +1,6 @@
+import { APP_KEYS } from "../modules/common/consts";
+
+export const logout = () => {
+  localStorage.removeItem(APP_KEYS.STORAGE_KEYS.TOKEN);
+  window.location.href = (APP_KEYS.ROUTER_KEYS.AUTH);
+};
